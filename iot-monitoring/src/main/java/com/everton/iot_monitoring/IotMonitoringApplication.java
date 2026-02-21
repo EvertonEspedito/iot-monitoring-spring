fe6fda25fc352306ce8b9d867c9387c28a90f2ba
Everton Espedito <evertondev505@gmail.com>
2026-02-21 15:49:30 -0300

    Iniciando Backend

---
+++ iot-monitoring/src/main/java/com/everton/iot_monitoring/IotMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.everton.iot_monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotMonitoringApplication.class, args);
+	}
+
+}
